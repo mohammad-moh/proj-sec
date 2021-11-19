91e626dae29b7c50443c165839f4769e013d021e
Mohammad <mohmmad.salamat84@gmail.com>
2021-11-19 20:49:53 +0200

    adding name to all routers

--- routes/web.php
+++ routes/web.php
@@ -15,19 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::get('/nav', function(){
     return view('layout.navbar');
-});
+})->name('navbar');
 
 Route::get('/home', function(){
     return view('layout.home');
-});
+})->name('home');
 
 Route::get('/about-me', function(){
     return view('layout.aboute_me');
-} );
+} )->name('about');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
